e61645a299dfc4ab58cbb62ed51b2e913b66026e
otodockal <oto.dockal@gmail.com>
2018-03-11 18:46:10 +0100

    build: add missing "typings" prop in order to avoid TS2.7 errors
    + bundle files have type suffixes
    + rollup config updated
    + index.js & index.d.ts removed

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,65 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-const pkg = require('./package.json')
-const { camelCase } = require('lodash')
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+const pkg = require('./package.json');
+const { camelCase } = require('lodash');
 
-const libraryName = 'subjective'
-
-export default {
-  input: `compiled/${libraryName}.js`,
-  output: [
-		{ file: pkg.main, name: camelCase(libraryName), format: 'umd' },
-		{ file: pkg.module, format: 'es' }
-  ],
-  sourcemap: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [
+const libraryName = 'subjective';
+const input = `compiled/${libraryName}.js`;
+const external = [
     'rxjs/BehaviorSubject',
     'rxjs/Observable',
-    'rxjs/operators/distinctUntilKeyChanged',
     'rxjs/operators/distinctUntilChanged',
     'rxjs/operators/map',
-  ],
-  globals: {
+];
+const globals = {
     'rxjs/BehaviorSubject': 'Rx',
     'rxjs/Observable': 'Rx',
-    'rxjs/operators/distinctUntilKeyChanged': 'Rx',
     'rxjs/operators/distinctUntilChanged': 'Rx',
     'rxjs/operators/map': 'Rx',
-  },
-  plugins: [
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-     // Allow node_modules resolution, so you can use 'external' to control
-     // which external modules to include in the bundle
-     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve()
-  ]
-}
+};
+
+export default [
+    // browser-friendly UMD build
+    {
+        input,
+        external,
+        output: {
+            name: camelCase(libraryName),
+            file: pkg.browser,
+            format: 'umd',
+            globals,
+            sourcemap: true,
+        },
+        plugins: [
+            resolve(), // so Rollup can find `ms`
+            commonjs(), // so Rollup can convert `ms` to an ES module
+        ],
+    },
+
+    // CommonJS (for Node) and ES module (for bundlers) build.
+    // (We could have three entries in the configuration array
+    // instead of two, but it's quicker to generate multiple
+    // builds from a single configuration where possible, using
+    // an array for the `output` option, where we can specify
+    // `file` and `format` for each target)
+    {
+        input,
+        external,
+        output: [
+            {
+                name: camelCase(libraryName),
+                file: pkg.main,
+                format: 'cjs',
+                globals,
+                sourcemap: true,
+            },
+            {
+                name: camelCase(libraryName),
+                file: pkg.module,
+                format: 'es',
+                globals,
+                sourcemap: true,
+            },
+        ],
+    },
+];
